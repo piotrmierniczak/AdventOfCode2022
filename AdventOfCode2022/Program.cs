cf3f20d9750bcb93d5920af14e0289192082b94d
PM <pm1975@windowslive.com>
2022-12-02 18:32:49 +0100

    first commit

---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventOfCode2022;
+
+Console.WriteLine("Task1");
+Task1.Run();
+Console.WriteLine();
---
+++ AdventOfCode2022/Task1.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode2022
+{
+    public static class Task1
+    {
+        public static void Run()
+        {
+            //https://adventofcode.com/2022/day/1
+            string[] lines = File.ReadAllLines("Task1File.txt");
+            int tempSum = 0;
+            List<int> list = new List<int>();
+            foreach (string line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    list.Add(tempSum);
+                    tempSum = 0;
+                    continue;
+                }
+                else
+                {
+                    int.TryParse(line, out int number);
+                    tempSum += number;
+                }
+            }
+            list = list.OrderByDescending(x => x).ToList();
+            long total = list[0] + list[1] + list[2];
+            Console.WriteLine($"Max calories: {list[0]}"); //69289
+            Console.WriteLine($"Total three max calories: {total}"); //205615
+        }
+    }
+}
