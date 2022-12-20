246dc043f0ef55ec3c00c5ca0f926da5edbefac4
macosami <pm1975@windowslive.com>
2022-12-20 17:52:42 +0100

    Third task done (part one and part two)

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -5,9 +5,32 @@ using AdventOfCode2022;
 //Task1.PartTwo();
 //Console.WriteLine();
 
-Console.WriteLine("Task2. Part One.");
-Task2.PartOne();
-Console.WriteLine();
-Console.WriteLine("Task2. Part Two.");
-Task2.PartTwo();
-Console.WriteLine();
+//Console.WriteLine("Task2. Part One.");
+//Task2.PartOne();
+//Console.WriteLine();
+//Console.WriteLine("Task2. Part Two.");
+//Task2.PartTwo();
+//Console.WriteLine();
+
+Task3.PartOne();
+string[] lines = File.ReadAllLines("TextFile3.txt");
+
+int result = 0;
+for (int i = 0; i < lines.Length; i += 3)
+{
+    string first = lines[i];
+    string second = lines[i+1];
+    string third = lines[i+2];
+    for (int j = 0; j < first.Length; j++)
+    {
+        char letter = first[j];
+        if(second.Contains(letter) && third.Contains(letter))
+        {
+            int number = letter - 0;
+            number -= number >= 97 ? 96 : 38; // a(97) - 96 = 1, A(65) - 38 = 27
+            result += number;
+            break;
+        }
+    }
+}
+Console.WriteLine(result);
---
+++ AdventOfCode2022/Task3.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode2022
+{
+	public class Task3
+	{
+        public static void PartOne()
+        {
+            Console.WriteLine("We know that:");
+            Console.WriteLine($"char a has number: {'a' - 0}");
+            Console.WriteLine($"char z has number: {'z' - 0}");
+            Console.WriteLine($"char A has number: {'A' - 0}");
+            Console.WriteLine($"char Z has number: {'Z' - 0}");
+            Console.WriteLine("so the solution is:");
+
+            string[] lines = File.ReadAllLines("TextFile3.txt");
+
+            int result = 0;
+            foreach (string line in lines)
+            {
+                int half = line.Length / 2;
+                string firstPart = line.Substring(0, half);
+                string secondPart = line.Substring(half, half);
+                for (int i = 0; i < firstPart.Length; i++)
+                {
+                    char letter = firstPart[i];
+                    if (secondPart.Contains(letter))
+                    {
+                        int number = letter - 0;
+                        number -= number >= 97 ? 96 : 38; // a(97) - 96 = 1, A(65) - 38 = 27
+                        result += number;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
